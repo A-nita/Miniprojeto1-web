1edca9fc76075f8b0894d41a96638c0415ed21e8
A_nita <nimoura1@gmail.com>
2023-01-25 20:00:04 -0300

    Carousel não tá totalmente quebrado
    Co-authored-by: Cinthia Costa <Pandxra@users.noreply.github.com>

--- js/carrossel.js
+++ js/carrossel.js
@@ -1,40 +1,44 @@
-const controleBotao = document.querySelectorAll('.btnControl') /* seleciona todos os botoes de controle */
-const elementos = document.querySelectorAll('.duplinha') /* seleciona todos os elementos  */
-const maxElementos = elementos.length
-let elementoAtivo = 0
 
-console.log(controleBotao)
-console.log(elementos)
-controleBotao.forEach((btnControl) => {
-    btnControl.addEventListener('click', () => {
-        console.log("numero de elemetos no carrosel " + maxElementos)
-        const arrowLeft = btnControl.classList.contains('btn_left')
+document.addEventListener('DOMContentLoaded', () => {
+    const controleBotao = document.querySelectorAll('.btnControl') /* seleciona todos os botoes de controle */
+    const elementos = document.querySelectorAll('.duplinha') /* seleciona todos os elementos  */
+    const maxElementos = elementos.length
+    let elementoAtivo = 0
 
-        /* checando qual é a imagem que vai ser inserida no carrossel */
-        console.log("elemento atual antes do click " + elementoAtivo)
-        if (arrowLeft) {
-            console.log("--")
-            elementoAtivo--
-        } else {
-            console.log("++")
-            elementoAtivo++
-        }
-        console.log("elemento atual depois do click " + elementoAtivo)
+    console.log(controleBotao)
+    console.log(elementos)
+    controleBotao.forEach((btnControl) => {
+        btnControl.addEventListener('click', () => {
+            console.log("numero de elemetos no carrosel " + maxElementos)
+            const arrowLeft = btnControl.classList.contains('btn_left')
 
-        /* corrigindo a possição do vetor de elementos de acordo com a quantidade de elementos */
-        if (elementoAtivo < 0) {
-            elementoAtivo = maxElementos - 1
-        } else if (elementoAtivo >= maxElementos - 1) {
-            elementoAtivo = 0
-        }
-        console.log(elementoAtivo)
-        elementos[elementoAtivo].scrollIntoView({
-            inline: "start",
-            behavior: "smooth"
+            /* checando qual é a imagem que vai ser inserida no carrossel */
+            console.log("elemento atual antes do click " + elementoAtivo)
+            if (arrowLeft) {
+                console.log("--")
+                elementoAtivo--
+            } else {
+                console.log("++")
+                elementoAtivo++
+            }
+            console.log("elemento atual depois do click " + elementoAtivo)
 
+            /* corrigindo a possição do vetor de elementos de acordo com a quantidade de elementos */
+            if (elementoAtivo < 0) {
+                elementoAtivo = maxElementos - 1
+            } else if (elementoAtivo >= maxElementos - 1) {
+                elementoAtivo = 0
+            }
+            console.log(elementoAtivo)
+            elementos[elementoAtivo].scrollIntoView({
+                inline: "start",
+                behavior: "smooth"
+
+            })
         })
     })
 })
+
 
 /*const btnCard = document.querySelectorAll('.card') /* seleciona todos os elementos de card */
 //const maxCards = btnCard.length
