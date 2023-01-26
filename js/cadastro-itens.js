e55bfd0780dc23d5b9e3338bbc8192b2073c7035
A_nita <nimoura1@gmail.com>
2023-01-26 17:30:45 -0300

    Tela de cadastro atualizada/ ta mais bunitinha

--- js/cadastro-itens.js
+++ js/cadastro-itens.js
@@ -1,19 +1,9 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-    const anoPublicacaoInput = document.getElementById('ano-publicacao')
-    const precoInput = document.getElementById('preco')
-    const emailInput = document.getElementById('email')
+    const anoPublicacaoSelect = document.getElementById('ano-publicacao')
 
-
+    let initYear = 1800
+    let currentYear = new Date().getFullYear() 
+    for (let year = initYear; i)
 })
 
-
-function ValidateEmail(mail) 
-{
- if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
-  {
-    return (true)
-  }
-    alert("You have entered an invalid email address!")
-    return (false)
-}
