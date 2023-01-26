38a6e74a01ea0c0443cdc04f46e5a0447f811959
A_nita <nimoura1@gmail.com>
2023-01-26 18:08:26 -0300

    Cadastro finalizado

--- js/cadastro-itens.js
+++ js/cadastro-itens.js
@@ -4,6 +4,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let initYear = 1800
     let currentYear = new Date().getFullYear() 
-    for (let year = initYear; i)
+    for (let year = initYear; year <= currentYear; year++) {
+        const newOption = document.createElement('option')
+        newOption.value = year
+        newOption.innerText = year
+        anoPublicacaoSelect.appendChild(newOption)
+    }
+
+    const formCadastroItens = document.getElementById('form-cadastro-item')
+    formCadastroItens.addEventListener('submit', (event) => {
+      alert('Cadastro do item realizado')
+    })
 })
 
